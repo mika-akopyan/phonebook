e85cb96a75f967a3929b164e3d89ed70099b40fb
mika_akopyan <mika.akopyan2505@gmail.com>
2024-02-12 12:37:36 +0300

    chore: Format the file according to the PEP8 standard

--- contact.py
+++ contact.py
@@ -1,13 +1,16 @@
 import re
 
+
 class Contact:
-    def __init__(self,
-                 surname:str = None,
-                 name:str = None,
-                 patronymic:str = None,
-                 organization:str = None,
-                 work_number = None,
-                 personal_number = None) -> None:
+    def __init__(
+        self,
+        surname: str = None,
+        name: str = None,
+        patronymic: str = None,
+        organization: str = None,
+        work_number = None,
+        personal_number = None
+    ) -> None:
         if surname is not None:
             self.surname = surname
 
@@ -25,64 +28,66 @@ class Contact:
 
         if personal_number is not None:
             self.personal_number = personal_number
-    
+
     @property
     def surname(self):
         return self.__surname
 
     @surname.setter
-    def surname(self, surname:str):
+    def surname(self, surname: str):
         # Проверка корректности типов данных
         if isinstance(surname, str):
             self.__surname = surname.strip()
         else:
-            raise TypeError('Ожидается строковое значение!')
+            raise TypeError("Ожидается строковое значение!")
 
     @property
     def name(self):
         return self.__name
 
     @name.setter
-    def name(self, name:str):
+    def name(self, name: str):
         # Проверка корректности типов данных
         if isinstance(name, str):
             self.__name = name.strip()
         else:
-            raise TypeError('Ожидается строковое значение!')
+            raise TypeError("Ожидается строковое значение!")
 
     @property
     def patronymic(self):
         return self.__patronymic
 
     @patronymic.setter
-    def patronymic(self, patronymic:str):
+    def patronymic(self, patronymic: str):
         # Проверка корректности типов данных
         if isinstance(patronymic, str):
             self.__patronymic = patronymic.strip()
         else:
-            raise TypeError('Ожидается строковое значение!')
-    
+            raise TypeError("Ожидается строковое значение!")
+
     @property
     def organization(self):
         return self.__organization
 
     @organization.setter
-    def organization(self, organization:str):
+    def organization(self, organization: str):
         # Проверка корректности типов данных
         if isinstance(organization, str):
             self.__organization = organization.strip()
         else:
-            raise TypeError('Ожидается строковое значение!')
+            raise TypeError("Ожидается строковое значение!")
 
     @property
     def work_number(self):
         return self.__work_number
 
     @work_number.setter
-    def work_number(self, work_number:str):
+    def work_number(self, work_number: str):
         # Проверка корректности типов данных
-        if re.fullmatch('8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}', work_number) is None:
-            raise TypeError('Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!')
+        if re.fullmatch("8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}", work_number) is None:
+            raise TypeError(
+                "Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!"
+            )
         else:
             self.__work_number = work_number
 
@@ -91,13 +96,20 @@ class Contact:
         return self.__personal_number
 
     @personal_number.setter
-    def personal_number(self, personal_number:str):
+    def personal_number(self, personal_number: str):
         # Проверка корректности типов данных
-        if re.fullmatch('8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}', personal_number) is None:
-            raise TypeError('Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!')
+        if (
+            re.fullmatch("8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}", personal_number)
+            is None
+        ):
+            raise TypeError(
+                "Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!"
+            )
         else:
             self.__personal_number = personal_number
 
     def save(self):
-        with open('phonebook.txt', 'a', encoding='utf-8') as file:
-            file.write(f'{self.surname, self.name, self.patronymic, self.organization, self.work_number, self.personal_number}')
+        with open("phonebook.txt", "a", encoding="utf-8") as file:
+            file.write(
+                f"{self.surname, self.name, self.patronymic, self.organization, self.work_number, self.personal_number}"
+            )
