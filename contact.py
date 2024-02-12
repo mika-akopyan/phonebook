a662aef62a5bc2a26f1293f5d682ca6ea897f227
mika_akopyan <mika.akopyan2505@gmail.com>
2024-02-12 10:49:27 +0300

    feat: Add validation of data types

--- contact.py
+++ contact.py
@@ -1,3 +1,5 @@
+import re
+
 class Contact:
     @property
     def surname(self):
@@ -5,7 +7,11 @@ class Contact:
 
     @surname.setter
     def surname(self, surname:str):
-        self.__surname = surname
+        # Проверка корректности типов данных
+        if isinstance(surname, str):
+            self.__surname = surname
+        else:
+            raise TypeError('Ожидается строковое значение!')
 
     @property
     def name(self):
@@ -13,7 +19,11 @@ class Contact:
 
     @name.setter
     def name(self, name:str):
-        self.__name = name
+        # Проверка корректности типов данных
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError('Ожидается строковое значение!')
 
     @property
     def patronymic(self):
@@ -21,7 +31,11 @@ class Contact:
 
     @patronymic.setter
     def patronymic(self, patronymic:str):
-        self.__patronymic = patronymic
+        # Проверка корректности типов данных
+        if isinstance(patronymic, str):
+            self.__patronymic = patronymic
+        else:
+            raise TypeError('Ожидается строковое значение!')
     
     @property
     def organization(self):
@@ -29,7 +43,11 @@ class Contact:
 
     @organization.setter
     def organization(self, organization:str):
-        self.__organization = organization
+        # Проверка корректности типов данных
+        if isinstance(organization, str):
+            self.__organization = organization
+        else:
+            raise TypeError('Ожидается строковое значение!')
 
     @property
     def work_number(self):
@@ -37,7 +55,11 @@ class Contact:
 
     @work_number.setter
     def work_number(self, work_number:str):
-        self.__work_number = work_number
+        # Проверка корректности типов данных
+        if re.fullmatch('8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}', work_number) is None:
+            raise TypeError('Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!')
+        else:
+            self.__work_number = work_number
 
     @property
     def personal_number(self):
@@ -45,4 +67,8 @@ class Contact:
 
     @personal_number.setter
     def personal_number(self, personal_number:str):
-        self.__personal_number = personal_number
+        # Проверка корректности типов данных
+        if re.fullmatch('8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}', personal_number) is None:
+            raise TypeError('Ожидается значение в формате 8-XXX-XXX-XX-XX, где X - любое число от 0 до 9!')
+        else:
+            self.__personal_number = personal_number
